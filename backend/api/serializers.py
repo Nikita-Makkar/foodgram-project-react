5c54681b1a7906e1549763e56f954860c0c5b3eb
Nikita-Makkar <134962780+Nikita-Makkar@users.noreply.github.com>
2024-03-11 09:15:13 +0300

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -18,14 +18,14 @@ class TagSerializer(serializers.ModelSerializer):
     """Сериализатор Теги"""
     class Meta:
         model = Tag
-        fields = "__all__"
+        fields = '__all__'
 
 
 class IngredientSerializer(serializers.ModelSerializer):
     """Сериализатор Ингредиенты"""
     class Meta:
         model = Ingredient
-        fields = "__all__"
+        fields = '__all__'
 
 
 class IngredientRecipesSerializer(serializers.ModelSerializer):
@@ -38,7 +38,7 @@ class IngredientRecipesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = IngredientRecipes
-        fields = ["id", "name", "measurement_unit", "amount"]
+        fields = ['id', 'name', 'measurement_unit', 'amount']
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -46,7 +46,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     image = Base64ImageField()
     tags = TagSerializer(read_only=True, many=True)
     ingredients = IngredientRecipesSerializer(
-        many=True, source="ingredientrecipes_set", read_only=True
+        many=True, source='ingredientrecipes_set', read_only=True
     )
     author = CustomUserSerializer(read_only=True)
     is_favorited = serializers.SerializerMethodField(read_only=True)
@@ -55,21 +55,21 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = [
-            "id",
-            "tags",
-            "author",
-            "ingredients",
-            "is_favorited",
-            "is_in_shopping_cart",
-            "name",
-            "image",
-            "text",
-            "cooking_time",
+            'id',
+            'tags',
+            'author',
+            'ingredients',
+            'is_favorited',
+            'is_in_shopping_cart',
+            'name',
+            'image',
+            'text',
+            'cooking_time',
         ]
 
     def get_is_favorited(self, obj):
         """Проверка наличия рецепта в избранном"""
-        user = self.context.get("request").user
+        user = self.context.get('request').user
         if user.is_anonymous:
             return False
         return Recipe.objects.filter(recipefavorites__user=user,
@@ -77,7 +77,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def get_is_in_shopping_cart(self, obj):
         """Проверка наличия рецепта в списке покупок"""
-        user = self.context.get("request").user
+        user = self.context.get('request').user
         if user.is_anonymous:
             return False
         return Recipe.objects.filter(shoppinglist__user=user,
@@ -86,7 +86,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         """Метод для валидации данных
         перед созданием рецепта
         """
-        ingredients = self.initial_data.get("ingredients")
+        ingredients = self.initial_data.get('ingredients')
         if not ingredients:
             raise serializers.ValidationError(
                 {"ingredients": "В рецепте отсутсвуют ингредиенты"}
@@ -94,20 +94,20 @@ class RecipeSerializer(serializers.ModelSerializer):
         ingredients_result = []
         for ingredient_item in ingredients:
             ingredient = get_object_or_404(Ingredient,
-                                           id=ingredient_item["id"]
+                                           id=ingredient_item['id']
                                            )
             if ingredient in ingredients_result:
                 raise serializers.ValidationError(
-                    "Ингредиент уже добавлен в рецепт"
+                    'Ингредиент уже добавлен в рецепт'
                 )
-            amount = ingredient_item["amount"]
-            if not (isinstance(ingredient_item["amount"], int)
-                    or ingredient_item["amount"].isdigit()):
-                raise ValidationError("Неправильное количество ингидиента")
-            ingredients_result.append({"ingredients": ingredient,
-                                       "amount": amount
+            amount = ingredient_item['amount']
+            if not (isinstance(ingredient_item['amount'], int)
+                    or ingredient_item['amount'].isdigit()):
+                raise ValidationError('Неправильное количество ингидиента')
+            ingredients_result.append({'ingredients': ingredient,
+                                       'amount': amount
                                        })
-        data["ingredients"] = ingredients_result
+        data['ingredients'] = ingredients_result
         return data
 
     def create_ingredients(self, ingredients, recipes):
@@ -115,34 +115,34 @@ class RecipeSerializer(serializers.ModelSerializer):
         IngredientRecipes.objects.bulk_create([
             IngredientRecipes(
                 recipes=recipes,
-                ingredients=ingredient["ingredients"],
+                ingredients=ingredient['ingredients'],
                 amount=ingredient.get("amount"),
             ) for ingredient in ingredients
         ])
 
     def create(self, validated_data):
         """Создание рецепта"""
-        image = validated_data.pop("image")
-        ingredients_data = validated_data.pop("ingredients")
+        image = validated_data.pop('image')
+        ingredients_data = validated_data.pop('ingredients')
         recipes = Recipe.objects.create(image=image, **validated_data)
-        tags = self.initial_data.get("tags")
+        tags = self.initial_data.get('tags')
         recipes.tags.set(tags)
         self.create_ingredients(ingredients_data, recipes)
         return recipes
 
     def update(self, instance, validated_data):
         """Обновление рецепта"""
-        instance.image = validated_data.get("image", instance.image)
-        instance.name = validated_data.get("name", instance.name)
-        instance.text = validated_data.get("text", instance.text)
+        instance.image = validated_data.get('image', instance.image)
+        instance.name = validated_data.get('name', instance.name)
+        instance.text = validated_data.get('text', instance.text)
         instance.cooking_time = validated_data.get(
-            "cooking_time", instance.cooking_time
+            'cooking_time', instance.cooking_time
         )
         instance.tags.clear()
-        tags_data = self.initial_data.get("tags")
+        tags_data = self.initial_data.get('tags')
         instance.tags.set(tags_data)
         IngredientRecipes.objects.filter(recipes=instance).all().delete()
-        self.create_ingredients(validated_data.get("ingredients"), instance)
+        self.create_ingredients(validated_data.get('ingredients'), instance)
         instance.save()
         return instance
 
@@ -159,9 +159,9 @@ class RecipeFavoritesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = RecipeFavorites
-        fields = ["id", "name", "image", "cooking_time"]
+        fields = ['id', 'name', 'image', 'cooking_time']
         validators = UniqueTogetherValidator(
-            queryset=RecipeFavorites.objects.all(), fields=("user", "recipes")
+            queryset=RecipeFavorites.objects.all(), fields=('user', 'recipes')
         )
 
 
@@ -171,17 +171,17 @@ class FollowRecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ["id", "name", "image", "cooking_time"]
-        read_only_fields = ["id", "name", "image", "cooking_time"]
+        fields = ['id', 'name', 'image', 'cooking_time']
+        read_only_fields = ['id', 'name', 'image', 'cooking_time']
 
 
 class FollowSerializer(serializers.ModelSerializer):
     """Сериализатор Подписки"""
-    email = serializers.ReadOnlyField(source="author.email")
-    id = serializers.ReadOnlyField(source="author.id")
-    username = serializers.ReadOnlyField(source="author.username")
-    first_name = serializers.ReadOnlyField(source="author.first_name")
-    last_name = serializers.ReadOnlyField(source="author.last_name")
+    email = serializers.ReadOnlyField(source='author.email')
+    id = serializers.ReadOnlyField(source='author.id')
+    username = serializers.ReadOnlyField(source='author.username')
+    first_name = serializers.ReadOnlyField(source='author.first_name')
+    last_name = serializers.ReadOnlyField(source='author.last_name')
     is_subscribed = serializers.SerializerMethodField()
     recipes = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()
@@ -189,14 +189,14 @@ class FollowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Follow
         fields = [
-            "email",
-            "id",
-            "username",
-            "first_name",
-            "last_name",
-            "is_subscribed",
-            "recipes",
-            "recipes_count",
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+            'recipes',
+            'recipes_count',
         ]
 
     def get_is_subscribed(self, obj):
@@ -207,8 +207,8 @@ class FollowSerializer(serializers.ModelSerializer):
 
     def get_recipes(self, obj):
         """Получение рецептов автора"""
-        request = self.context.get("request")
-        limit = request.GET.get("recipes_limit")
+        request = self.context.get('request')
+        limit = request.GET.get('recipes_limit')
         queryset = Recipe.objects.filter(author=obj.author)
         if limit:
             queryset = queryset[: int(limit)]
@@ -230,9 +230,9 @@ class ShoppingListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ShoppingList
-        fields = ["id", "name", "image", "cooking_time"]
+        fields = ['id', 'name', 'image', 'cooking_time']
         validators = [
             UniqueTogetherValidator(
-                queryset=ShoppingList.objects.all(), fields=("user", "recipes")
+                queryset=ShoppingList.objects.all(), fields=('user', 'recipes')
             )
         ]
